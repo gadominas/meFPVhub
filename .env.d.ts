c52afa33c5d932f1c38b7f8b7edcbde8eea50fcd
gadominas@gmail.com <gsiutilas@eisgroup.com>
2021-04-19 14:25:22 +0300

    initial version of mefpv

---
+++ .env.d.ts
@@ -0,0 +1,7 @@
+declare namespace NodeJS {
+  export interface ProcessEnv {
+    NEXT_PUBLIC_FIREBASE_API_KEY: string;
+    NEXT_PUBLIC_FIREBASE_PROJECT_ID: string;
+    NEXT_APP_GOOGLE_MAPS_APIE_KEY: string;
+  }
+}
