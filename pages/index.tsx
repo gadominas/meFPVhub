9e66450affa38f9863b702bb3c1017a050d6ef32
gadominas@gmail.com <gsiutilas@eisgroup.com>
2021-04-19 15:24:05 +0300

    styles removed

--- pages/index.tsx
+++ pages/index.tsx
@@ -42,11 +42,10 @@ const center = {
 
 const db = firebase.database();
 const libraries = ["places"];
-const [markers, setMarkers] = useState([] as any);
 
 export default function Home() {
   //console.log(process.env.NEXT_APP_GOOGLE_MAPS_APIE_KEY);
-
+  const [markers, setMarkers] = useState([]);
   const { isLoaded, loadError } = useLoadScript({
     googleMapsApiKey: "AIzaSyAvY9qc9qANpIAvCX3MoSJfwjsMx_Ns_uQ",
     libraries: ["places"],
@@ -78,22 +77,23 @@ export default function Home() {
           center={center}
           //options={options}
           onClick={(event) => {
-            setMarkers((current: any) => [
-              ...current,
-              {
-                lat: event.latLng.lat(),
-                lng: event.latLng.lng(),
-                time: new Date(),
-              },
-            ]);
+            console.log(event);
+            // setMarkers((current) => [
+            //   ...current,
+            //   {
+            //     lat: event.latLng.lat(),
+            //     lng: event.latLng.lng(),
+            //     time: new Date(),
+            //   },
+            // ]);
           }}
         >
-          {markers.map((marker) => (
+          {/* {markers.map((marker) => (
             <Marker
               key={marker.time.toISOString()}
               position={{ lat: marker.lat, lng: marker.lng }}
             />
-          ))}
+          ))} */}
         </GoogleMap>
       </div>
     </main>
